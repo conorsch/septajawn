b3e00184231201716d93eacd0a23f924089b1ee7
Conor Schaefer <conor.schaefer@gmail.com>
2014-05-01 18:18:32 -0400

    adds dynamic servername for use in templates (for JS calls)

--- septajawn/settings.py
+++ septajawn/settings.py
@@ -127,6 +127,11 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.request',
+)
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
